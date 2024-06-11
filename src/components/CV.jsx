79ced66042f530aca51a1f309ba0297a8a5be2d1
ArnoldMsl <arnold.masselin@gmail.com>
2024-06-11 10:10:06 +0200

    working extends directory

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -4,29 +4,51 @@ import { useState } from 'react';
 
 const CV = () => {
 
-    const [isVisible, setIsVisible] = useState(false);
-    const [isRotated, setIsRotated] = useState(false);
+    const [visibleSections, setVisibleSections] = useState([false, false, false]);
 
-    const toggleVisibility = () => {
-        setIsVisible(!isVisible)
-        setIsRotated(!isRotated);
+    const toggleVisibility = (index) => {
+        setVisibleSections((prevVisibleSections) =>
+            prevVisibleSections.map((visible, i) => (i === index ? !visible : visible))
+        );
     };
 
     return (
         <div className="cvContainer">
             <div className="descriptionContainer">
                 <div className="textCVContainer" id="hoard">
-                    <span>Avril-Juin 2024 : Développeur Front React - The Hoard</span>
-                    <p>Réalisation d'une application web de traitement d'images et d'impressions pour du jeu de rôle</p>
+                    <button onClick={() => toggleVisibility(0)}>
+                        Avr-Juil 24 : Développeur Front - The Hoard
+                        <img id="extendButton" src='../src/assets/images/extend-button.png' alt="étendre le texte" className={visibleSections[0] ? 'rotated' : ''}></img>
+                    </button>
+                    {visibleSections[0] && (
+                        <p>
+                            Réalisation pour une plateforme d'E-commerce, d'une application web mono-page de traitement d'images et d'impression en <span>REACT</span>
+                        </p>
+                    )}
                 </div>
                 <div className="textCVContainer" id="grdf">
-                    <button onClick={toggleVisibility}>
-                        2018 - 2023 : Acheteur - GRDF
-                        <img id="extendButton" src='../src/assets/images/extend-button.png' alt="étendre le texte" className={isRotated ? 'rotated' : ''}></img>
+                    <button onClick={() => toggleVisibility(1)}>
+                        2018 - 23 : Acheteur - GRDF
+                        <img id="extendButton" src='../src/assets/images/extend-button.png' alt="étendre le texte" className={visibleSections[1] ? 'rotated' : ''}></img>
                     </button>
-                    {isVisible && (
+                    {visibleSections[1] && (
                         <p>
-                            Formulation des besoins, établissement des stratégies d’achat, mise en place et suivi des consultations, négociations commerciales et contractuelles, gestion des catégories d’achats dédiées.
+                            Formulation des besoins, établissement des stratégies d’achat, mise en place et suivi des consultations, négociations commerciales et contractuelles, 
+                            gestion des catégories d’achats dédiées. Projets notables : <br /><br />
+                            -Gestion de la <span>catégorie d'achat Digitale</span> : Prestations intelectuelles, applications Saas <br /><br />
+                            -Achat de <span>Gaz</span> : Refonte de la structure contractuelle et concurrentielle en période de crise d’approvisionnement<br /><br />
+                            -Achat de produits <span>COVID</span> : Sourcing Fournisseur et suivi logistique en période de crise sanitaire.
+                        </p>
+                    )}
+                </div>
+                <div className="textCVContainer" id="sushis">
+                    <button onClick={() => toggleVisibility(2)}>
+                        2013 - 15 : Reponsable Restaurant - Pop sushis
+                        <img id="extendButton" src='../src/assets/images/extend-button.png' alt="étendre le texte" className={visibleSections[2] ? 'rotated' : ''}></img>
+                    </button>
+                    {visibleSections[2] && (
+                        <p>
+                            Job étudiant à temps partiel : Gestion de l'équipe, commandes fournisseurs, comptabilité basique, accueil, caisse, livraisons ponctuelles
                         </p>
                     )}
                 </div>
