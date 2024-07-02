61d48542e266a3e71026381f012c227b58a68888
ArnoldMsl <arnold.masselin@gmail.com>
2024-07-02 09:11:57 +0200

    fixed CSS issues on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,18 +18,19 @@ const Home = () => {
             h-36 mb-2
             sm:h-32 sm:m-2
             md:hidden"
-            id='photoProfil' src='../src/assets/images/photo.jpg' alt='photo de profil'></img>
+                id='photoProfil' src='../src/assets/images/photo.jpg' alt='photo de profil'></img>
 
             <div className="welcomeContainer inline-flex items-center">
 
                 <div className="welcomeText flex flex-col 
-                justify-center items-center">
+                justify-center items-center
+                xl:items-start">
                     <h2 className="
                     text-5xl
                     sm:text-6xl
-                    md:items-start md:text-7xl md:ml-20
+                    md:items-start md:justify-start md:text-7xl md:ml-20
                     lg:text-8xl lg:ml-28 lg:mt-10
-                    xl:ml-44 xl:mt-0
+                    xl:mt-0 xl:mb-0
                     ">ARNOLD MASSELIN</h2>
                     <p className="
                     text-center
@@ -37,14 +38,14 @@ const Home = () => {
                     sm:text-3xl sm:mt-4 sm:mb-2 sm:mr-8 sm:ml-8
                     md:text-start md:text-4xl md:ml-20 md:mt-10 md:mb-10
                     lg:text-5xl lg:ml-28
-                    xl:text-6xl xl:ml-44
+                    xl:text-6xl
                     ">Et en tant que <span>Développeur Web,</span> je créé des trucs sur Internet</p>
                 </div>
                 <img className="rounded-full
                 hidden
                 md:block md:h-44 md:mr-10
                 lg:h-56 lg:mr-10
-                xl:h-80 xl:m-10"
+                xl:h-80 xl:mr-10"
                     id='photoProfil' src='../src/assets/images/photo.jpg' alt='photo de profil'></img>
             </div>
             <p className="
