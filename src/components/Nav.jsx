e8e39fdf16a1e56bac532d5e7f4c57e2ea9906f5
ArnoldMsl <arnold.masselin@gmail.com>
2024-07-03 08:47:55 +0200

    little fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
                 className="
                 flex items-center justify-center
                 w-screen h-24 mt-5 mb-5
-                md:h-1/3 md:w-1/3 md:mr-auto md:ml-auto md:mt-0 md:mb-0
+                md:h-1/3 md:w-1/3 md:mr-auto md:ml-auto md:mt-0 md:mb-0 md:pt-[-10%]
                 navButtonSmall">
                 <div className="flex justify-center 
                 md:w-60 md:h-60"
@@ -26,7 +26,8 @@ const Nav = () => {
                 </div>
             </Link>
 
-            <div className="md:flex md:justify-center md:items-center"
+            <div className="md:flex md:justify-center md:items-center
+            md:h-1/3"
                 id="navCenterRow">
 
                 <Link to="/CV"
@@ -49,7 +50,7 @@ const Nav = () => {
 
                 <p className="flex w-screen justify-center items-center
                 text-4xl drop-shadow-sm mt-12 mb-12
-                md:w-96 md:text-5xl md:ml-8 md:mr-14 md:text-center"
+                md:w-96 md:text-5xl md:ml-5 md:mr-9 md:text-center md:mt-0 md:mb-0"
                     id="navText">
                     On commence où ?</p>
 
