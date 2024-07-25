8ef685796e8b3bbd99d6d83f2db0f8d94718e01c
ArnoldMsl <arnold.masselin@gmail.com>
2024-07-25 10:05:18 +0200

    minor CSS adjustments

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,39 +17,35 @@ const Projects = () => {
     {
       title: "Minis-printer",
       description: "Application dédiée au jeux de rôle qui permet à un utilisateur d'uploader ses images pour créer des présentoirs à imprimer sur des feuilles A4",
-      descriptionDesktop: "Application réalisée dans le cadre d'un stage pour l'entreprise The hoard qui vend des accessoires pour le jeux de rôle papier Donjons & Dragons. \n L'application permet à l'utilisateur d'uploader ses propres images puis les customiser pour créer des présentoirs en papier à imprimer",
+      descriptionDesktop: "Application réalisée dans le cadre d'un stage pour l'entreprise The hoard qui vend des accessoires pour le jeux de rôle papier Donjons & Dragons. \n L'application permet à l'utilisateur d'uploader ses propres images puis les customiser pour créer des présentoirs en papier à imprimer.",
       technology: "#React, #Tailwind",
       lien: "https://thehoard.github.io/",
       github: "",
       images: ["image1.png", "image2.png"],
-      index: '1'
     },
     {
       title: "GhostQuill",
       description: "Projet personnel de site de partage de textes anonymes",
-      descriptionDesktop: "",
+      descriptionDesktop: "Projet personnel de site de partages de textes anonymes en cours de réalisation sur une architecture Symfony SQL et react",
       technology: "#Symfony, #SQL, #React, #TailWind",
       github: "https://github.com/ArnoldMsl/GhostQuill",
       images: ["image1.png", "image2.png"],
-      index: '2'
     },
     {
       title: "Origins Digital",
       description: "Projet d'école réalisé à 4 en 6 semaines : Site de partage de vidéos avec système de gestion des profils utilisateurs et de paiement en ligne",
-      descriptionDesktop: "",
+      descriptionDesktop: "Projet d'école réalisé à 4 en 6 semaines : Plateforme d'enseignement au développement Web via des vidéos. Le site comporte la gestion des comptes utilisateurs lesquels disposent d'un planning d'apprentissage généré automatiquement via un Quiz. En outre, le site propose une interface de paiement gérée par Stripe.",
       technology: "#Symfony, #SQL, #JavaScript, #Stripe API",
       github: "https://github.com/WildCodeSchool-2023-09/php-paris-p3-originsdigital",
       images: ["image1.png", "image2.png", "image3.png"],
-      index: '3'
     },
     {
       title: "Cyclaid",
       description: "Projet d'école réalisé à 4 en 6 semaines : Appli web mobile de partage de pièces détachées de vélos entre particuliers",
-      descriptionDesktop: "",
+      descriptionDesktop: "Projet d'école réalisé à 4 en 6 semaines via SQL et une architecture MVC : Appli web mobile de partage de pièces détachées de vélos entre particuliers. Le site dispose d'un système de gestion des utilisateurs",
       technology: "#MVC, #PHP, #SQL",
       github: "https://github.com/WildCodeSchool-2023-09/PHP-paris-p2-cyclaid",
       images: ["image1.png", "image2.png"],
-      index: '4'
     },
   ]
 
@@ -167,10 +163,11 @@ const Projects = () => {
                   <div className="flex flex-col justify-start items-center content-center text-center
                     pt-2 pb-2 w-11/12 mr-auto ml-auto border-t-2 border-b-2 border-secondaryMinor
                     lg:rounded-lg lg:border-8 lg:border-double
-                    lg:grid-cols-2 lg:p-2 
+                    lg:grid-cols-2 lg:p-4
+                    2xl:w-10/12 
                     project">
-                    <h3 id="projectTitle" className="text-3xl lg:text-4xl">{project.title}</h3>
-                    <p id="projectDescription" className="text-lg lg:text-xl lg:mt-2">{project.descriptionDesktop}</p>
+                    <h3 id="projectTitle" className="text-3xl lg:text-5xl 2xl:text-7xl">{project.title}</h3>
+                    <p id="projectDescription" className="text-lg lg:text-xl lg:mt-2 2xl:text-2xl">{project.descriptionDesktop}</p>
 
                     {project.images && project.images.length > 0 && (
                       <ImageCarousel images={project.images.map(img => `${project.title}/${img}`)} />
@@ -179,20 +176,20 @@ const Projects = () => {
                     {project.technology && (
                       <div id="projectTechnology" className="flex flex-wrap justify-center items-center lg:mt-2 mr-auto ml-auto">
                         <img className="w-6 mr-2" src="src/assets/images/general_icons/cog.svg"></img>
-                        <p className="text-lg lg:text-2xl">Technologies : {project.technology}</p>
+                        <p className="text-lg lg:text-2xl">{project.technology}</p>
                       </div>
                     )}
 
-                    <div id="projectLinks" className="flex justify-center">
+                    <div id="projectLinks" className="flex justify-center 2xl:p-2">
                       {project.lien && (
-                        <a className="flex justify-evenly items-center p-2 rounded-lg text-3xl mt-2 lg:w-fit cvButtons" href={project.lien} target="_blank" rel="noopener noreferrer">
+                        <a className="flex justify-evenly items-center p-2 rounded-lg text-3xl mt-2 lg:w-fit 2xl:text-4xl 2xl:mt-4 cvButtons" href={project.lien} target="_blank" rel="noopener noreferrer">
                           Visiter le site
-                          <img className="w-6 ml-2" src="src/assets/images/general_icons/link.svg"></img>
+                          <img className="w-6 ml-2 2xl:w-10" src="src/assets/images/general_icons/link.svg"></img>
                         </a>
                       )}
                       {project.github && (
                         <p>
-                          <a className="flex justify-evenly items-center p-2 rounded-lg text-3xl lg:w-fit mt-2 cvButtons" href={project.github} target="_blank" rel="noopener noreferrer">
+                          <a className="flex justify-evenly items-center p-2 rounded-lg text-3xl lg:w-fit mt-2 2xl:text-4xl 2xl:mt-4 cvButtons" href={project.github} target="_blank" rel="noopener noreferrer">
                             Voir sur GitHub
                           </a>
                         </p>
@@ -207,11 +204,11 @@ const Projects = () => {
 
         <div className="hidden h-1/6 lg:inline-flex projectButtonsContainer mr-auto ml-auto">
           <button className="prev" onClick={prevSlide}>
-            <img className="rotate-90" src="src/assets/images/general_icons/extend-button.png"></img>
+            <img className="h-5/6 projectCarouselButton" src="src/assets/images/general_icons/left-button.png"></img>
           </button>
           <div className="h-full w-3/6 Lever"></div>
           <button className="next" onClick={nextSlide}>
-            <img className="-rotate-90" src="src/assets/images/general_icons/extend-button.png"></img>
+            <img className="h-5/6 projectCarouselButton" src="src/assets/images/general_icons/right-button.png"></img>
           </button>
         </div>
       </div>
