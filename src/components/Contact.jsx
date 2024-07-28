d9589a8ddb9d5ae534943883d8acfb791ef45409
ArnoldMsl <arnold.masselin@gmail.com>
2024-07-28 07:58:29 +0200

    added contact modal

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Contact = () => {
+
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     message: ''
-  });
+  })
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isFailModalOpen, setIsFailModalOpen] = useState(false)
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setFormData({
       ...formData,
       [name]: value
-    });
-  };
+    })
+  }
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     try {
       const response = await axios.post('https://api.arnoldmasselin.fr/', formData, {
@@ -25,12 +29,19 @@ const Contact = () => {
           'Content-Type': 'application/json'
         },
         withCredentials: true
-      });
-      console.log('Email sent:', response.data);
+      })
+      setIsModalOpen(true)
+      console.log('Email sent:', response.data)
     } catch (error) {
-      console.error('Error sending email:', error);
+      setIsFailModalOpen(true)
+      console.error('Error sending email:', error)
     }
-  };
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setIsFailModalOpen(false)
+  }
 
   return (
     <section>
@@ -61,8 +72,29 @@ const Contact = () => {
           <button type="submit" className="py-3 px-5 text-lg font-medium text-center text-white border-4 rounded-xl bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">C'est parti !</button>
         </form>
       </div>
-    </section>
-  );
-};
 
-export default Contact;
+      {isModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-primarycolor p-6 rounded-lg shadow-lg max-w-sm w-full">
+            <h3 className="text-lg font-bold mb-4">Message bien reçu !</h3>
+            <p className="mb-4 text-center">Merci de m'avoir contacté, je reviendrai vers vous au plus vite</p>
+            <button onClick={closeModal} className="flex py-2 px-4 border-white border-solid rounded-xl border-2 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 mr-auto ml-auto">Ok</button>
+          </div>
+        </div>
+      )}
+
+      {isFailModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-primarycolor p-6 rounded-lg shadow-lg max-w-sm w-full">
+            <h3 className="text-lg font-bold mb-4">Oops un problème est survenu...</h3>
+            <p className="mb-4 text-center">Vous pouvez me contacter via contact@arnoldmasselin.fr</p>
+            <button onClick={closeModal} className="flex py-2 px-4 border-white border-solid rounded-xl border-2 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 mr-auto ml-auto">Ok</button>
+          </div>
+        </div>
+      )}
+
+    </section>
+  )
+}
+
+export default Contact
